834fdca551b6d4f74fc4aab78bc2d23fea44139e
Milos Maksimovic <maksapn@gmail.com>
2018-12-02 10:05:26 +0100

    Tied in note component in notes. Minor styling.

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Note } from '../../models/note';
 import { NoteService } from '../../services/note.service';
@@ -12,12 +13,25 @@ export class NotesComponent implements OnInit {
 
   notes: Note[] = [];
 
-  constructor(private noteService: NoteService) { }
+  constructor(
+    private noteService: NoteService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
     this.noteService.getNotes().subscribe(notes => {
       this.notes = notes;
     });
+  }
+
+  onEditClicked(note: Note) {
+    this.router.navigate([{ outlets: { note: [note.id] } }], { relativeTo: this.activatedRoute });
+  }
+
+  onDeleteClicked(note: Note) {
+    const noteToDelete = this.notes.filter(n => n.id === note.id)[0];
+    this.notes.splice(this.notes.indexOf(noteToDelete), 1);
   }
 
 }
