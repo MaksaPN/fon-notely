6f98468063f903964c069c03fdf74b05901e1c8f
Milos Maksimovic <maksapn@gmail.com>
2018-12-02 09:48:11 +0100

    Completed search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'fon-search',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  searchTerm: string;
+  @Output() search = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSearchClick() {
+    this.search.emit(this.searchTerm);
   }
 
 }
